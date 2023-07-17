c31540db5021f46c3aae7fdd839ab76a20a0b859
Yoann Thébault <thebaultyoann@gmail.com>
2023-07-17 16:41:52 +0100

    update/activate/deactivate user CLI

--- database.py
+++ database.py
@@ -61,7 +61,7 @@ def update_user(session, username, newUsername, password, disabled):
     code=[]
     if user:
         print(disabled)
-        if disabled:
+        if disabled===bool:
             user.disabled = disabled
             code.append("1")
         if password:
