b6d87c4f34571749a507c87aa18596b47cebceb3
Yoann Thébault <thebaultyoann@gmail.com>
2023-07-19 15:59:34 +0100

    date_expiration

--- database.py
+++ database.py
@@ -1,11 +1,11 @@
-from sqlalchemy import create_engine, Column, Integer, String, Boolean, Date, JSON
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, Date
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
 class User(Base):
-    __tablename__ = "users2"
+    __tablename__ = "user"
     id = Column(Integer, primary_key=True)
     username = Column(String)   
     password_hashed = Column(String)
