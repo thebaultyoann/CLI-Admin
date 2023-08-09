d7d5857f94445938cb462bd0eb171266eb8c4468
Yoann Thébault <thebaultyoann@gmail.com>
2023-08-09 15:57:35 +0100

    new test

--- get_mariadb_ip.sh
+++ get_mariadb_ip.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo $(sudo docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mariadb > mariadb_ip)
+sudo docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mariadb > mariadb_ip
