e101c80d16e5a42b046581e869c3abcb920171ff
Yoann Thébault <thebaultyoann@gmail.com>
2023-08-09 16:03:52 +0100

    test

--- test.py
+++ test.py
@@ -1,14 +1,2 @@
-import datetime
-
-def convert_string_to_date(date):
-    try:
-        year, month, day = date.split('/')
-        new_date = datetime.date(int(year), int(month), int(day))
-        return new_date
-    except (ValueError, AttributeError):
-        return
-    except:
-        return
-    
-print(convert_string_to_date("2023/05/08"))
-print(datetime.date(int("2023"),int("05"),int("08")))
+with open("mariadb_ip", "r") as f:
+    database_ip = f.readline()
