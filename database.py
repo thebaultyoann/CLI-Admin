5f6ebce5d0b92088499528eb7702a9eca081df87
Yoann Thébault <thebaultyoann@gmail.com>
2023-08-09 17:21:56 +0100

    update

--- database.py
+++ database.py
@@ -143,7 +143,6 @@ def change_expiration_date(session: Session, username, expirationDate):
 def user_change_password(session : Session, username, password):
     user = session.query(User).filter(User.username == username).first()
     user.password = password
-    session.add(user)
     session.commit()
     return True
 
