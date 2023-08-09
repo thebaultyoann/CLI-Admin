e5d22dc5bd6effe5f3147b5f471025e172cd24cd
Yoann Thébault <thebaultyoann@gmail.com>
2023-08-09 14:33:56 +0100

    small update on auto_logout

--- cli.py
+++ cli.py
@@ -467,7 +467,7 @@ def connect_to_db():
     return session
 
 def auto_logout():
-    time.sleep(600)
+    time.sleep(10)
     os.putenv('username', '')
     os.putenv('password', '')
     typer.secho(f"You are now disconnected, if you want to keep using the CLI you need to reconnect", fg=typer.colors.RED)
