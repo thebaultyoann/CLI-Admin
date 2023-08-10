5e4c30116cfefd19fd959ac8aa534aef1d0ed437
Yoann Thébault <thebaultyoann@gmail.com>
2023-08-10 17:49:41 +0100

    sometest

--- cli_first_run.sh
+++ cli_first_run.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 sudo apt-get update
-sudo apt-get install python3 gcc wget libmariadb3 libmariadb-dev
+sudo apt-get install python3 python3-venv gcc wget libmariadb3 libmariadb-dev
 sudo wget https://downloads.mariadb.com/Connectors/c/connector-c-3.3.5/mariadb-connector-c-3.3.5-debian-buster-amd64.tar.gz -O - | sudo tar -zxf - --strip-components=1 -C /usr
 sudo echo "export LD_LIBRARY_PATH=/usr/lib/mariadb" >> ~/.bashrc    
 source ~/.bashrc
