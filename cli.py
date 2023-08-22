fe3c6c2553940adaad3faacb4f37ae26a544a1ba
Yoann Thébault <thebaultyoann@gmail.com>
2023-08-22 10:46:46 +0100

    password not hashed

--- cli.py
+++ cli.py
@@ -56,9 +56,6 @@ def user_authentificated(username:str, password:str):
     if username==None or password==None:
         typer.secho(f"You need to login", fg=typer.colors.RED)
         return False
-    password = get_password_hash(password)
-    password = password[:32]
-    print(password)
     try: 
         global database_ip
         global database_name
@@ -92,7 +89,6 @@ def login(
         Connect you to the CLI.
         Automatic disconnection in 10 minutes
     '''
-    password = get_password_hash(password)
     if user_authentificated(username=username, password=password):
         os.putenv('username',f'{username}')
         os.putenv('password',f'{password}')
@@ -283,7 +279,6 @@ def user_list(session):
     return True
 
 def user_add(session, username:str, password:str, activated:bool, expirationDate:str):
-    password = get_password_hash(password)
     if expirationDate: 
         expirationDate = convert_string_to_date(expirationDate)
         if not expirationDate:
@@ -348,8 +343,7 @@ def user_update(session, username:str, newUsername:str, password:str,  activated
         else:
             typer.secho(f"User {username} : the new expiration date is before today. If you want to perform this operation use the command changedate",fg=typer.colors.RED)
             check.append(4)
-    if password:
-        password = get_password_hash(password)        
+    if password:      
         if database.update_user_password(session=session, username=username, password=password):
             typer.secho(f"User {username} password has been changed", fg=typer.colors.GREEN)
         else: 
@@ -405,7 +399,6 @@ def user_change_expiration_date(session, username:str, expirationDate:str):
 
 def user_change_password(session, username, password):
     check_username(session, username)
-    password = get_password_hash(password)
     if database.user_change_password(session=session, username=username, password=password):
         typer.secho(f"Password of {username} was changed", fg=typer.colors.GREEN)
     else:
@@ -465,7 +458,6 @@ def get_password_hash(password):
 def connect_to_db():
     username=os.getenv('username')  
     password=os.getenv('password')
-    password = get_password_hash(password) #2nd hash of the admin password used to connect to the DB
     global database_ip
     global database_name
     session = database.start_a_db_session(
